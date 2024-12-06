9a4d9c17ddddb4882828dc8f710be77a4283dff6
sandeeq2404 <sandeepkumaar193@gmail.com>
2024-12-05 20:09:52 -0800

    authentication system added

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bcrypt = require("bcrypt")
+const app = express();
+app.use(express.json())
+
+const users = [
+
+]
+
+app.get("/users", (req,res)=>{
+    res.json(users)
+});
+
+app.post("/users", async (req, res) => {
+    try {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(req.body.password, salt); // Corrected line
+      console.log(salt);
+      console.log(hashedPassword);
+      const user = { name: req.body.name, password: hashedPassword };
+      users.push(user);
+      res.status(201).send();
+    } catch {
+      res.status(500).send();
+    }
+  });
+  
+app.listen(3001);   
